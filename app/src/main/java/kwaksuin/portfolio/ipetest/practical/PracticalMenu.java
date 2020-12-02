dc3dfca92b01e462dfb9e40f7af5fa521f6ae656
KwakSuin <mygirls0@naver.com>
2020-12-02 16:55:14 +0900

    Information Processing Engineer

---
+++ app/src/main/java/kwaksuin/portfolio/ipetest/practical/PracticalMenu.java
@@ -0,0 +1,14 @@
+package kwaksuin.portfolio.ipetest.practical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.practical_menu);
+    }
+}
---
+++ app/src/main/java/kwaksuin/portfolio/ipetest/write/WriteMenu.java
@@ -0,0 +1,16 @@
+package kwaksuin.portfolio.ipetest.write;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kwaksuin.portfolio.ipetest.R;
+
+public class WriteMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.write_menu);
+    }
+}
