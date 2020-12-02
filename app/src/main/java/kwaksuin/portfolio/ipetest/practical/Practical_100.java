021074385740fbd0be23430b6c8f38e04fa31d47
KwakSuin <mygirls0@naver.com>
2020-12-02 18:21:52 +0900

    Information Processing Engineer Practice

---
+++ app/src/main/java/kwaksuin/portfolio/ipetest/practical/Practical_100.java
@@ -0,0 +1,16 @@
+package kwaksuin.portfolio.ipetest.practical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kwaksuin.portfolio.ipetest.R;
+
+public class Practical_100 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.practical_100);
+    }
+}
