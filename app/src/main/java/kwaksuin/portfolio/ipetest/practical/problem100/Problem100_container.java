90e9f38c7b4481dc41861d84badaee29afcdfc8b
KwakSuin <mygirls0@naver.com>
2020-12-13 15:58:27 +0900

    Information Processing Engineer Practice

---
+++ app/src/main/java/kwaksuin/portfolio/ipetest/practical/problem100/Problem100_container.java
@@ -0,0 +1,16 @@
+package kwaksuin.portfolio.ipetest.practical.problem100;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kwaksuin.portfolio.ipetest.R;
+
+public class Problem100_container extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.problem100_container);
+    }
+}
