8494dc536724391ecb512fc0d5a16023818fc262
KwakSuin <mygirls0@naver.com>
2020-12-30 16:42:44 +0900

    Information Processing Engineer Practice

---
+++ app/src/main/java/kwaksuin/portfolio/ipetest/write/exam/WriteExam_2020_01_3.java
@@ -0,0 +1,23 @@
+package kwaksuin.portfolio.ipetest.write.exam;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kwaksuin.portfolio.ipetest.R;
+
+public class WriteExam_2020_01_3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootview = (ViewGroup)inflater.inflate(R.layout.writeexam_2020_01_3, container, false);
+
+
+        return rootview;
+    }
+}
